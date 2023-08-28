45e71eeeb8c334bb78faa4a5d7ff64ceec7ce01b
ne5oo <106486790+neo5oo@users.noreply.github.com>
2023-08-28 14:26:52 +0300

    Добавление задачи

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('.todo__form');
+const taskInput = document.querySelector('.todo__input');
+const tasksList = document.querySelector('.tasks');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value
+
+    const taskHTML = `<li class="task">
+    <h2 class="task__title">${taskText}</h2>
+    <div class="task__container">
+      <button type="button" class="button button_type_filled" aria-label="Отметить выполненной"></button>
+      <button type="button" class="button button_type_remove" aria-label="Удалить"></button>
+    </div>
+  </li>`
+
+  tasksList.insertAdjacentHTML('beforeend', taskHTML);
+})
+
